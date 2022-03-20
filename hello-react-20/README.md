908b12af7a59ec16b0fc67c001b25cf516a62b61
oh29oh29 <oh29oh29.develop@gmail.com>
2022-03-20 21:18:48 +0900

    [Hello React 20] Practice - Movie App

---
+++ hello-react-20/README.md
@@ -0,0 +1,7 @@
+# Hello React 20
+
+### 주요 내용
+
+Practice
+
+- Movie App
---
+++ hello-react-20/src/routes/Detail.js
@@ -0,0 +1,19 @@
+import {useParams} from "react-router-dom";
+import {useEffect} from "react";
+
+function Detail() {
+    const {id} = useParams();
+    const getMovie = async () => {
+        const response = await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`);
+        const json = await response.json();
+        console.log(json);
+    }
+
+    useEffect(() => {
+        getMovie();
+    }, []);
+
+    return <h1>Detail</h1>
+}
+
+export default Detail;
